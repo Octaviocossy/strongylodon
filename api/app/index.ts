6d602d5cc5a0ea08c34df13ee2c302f675c410e0
Octaviocossy <octaviocossytorquati@gmail.com>
2023-06-19 11:39:03 -0300

    Configurate CORS credentials 👮‍♂️

--- api/app/index.ts
+++ api/app/index.ts
@@ -9,7 +9,7 @@ const app = express();
 
 config();
 app.use(express.json());
-app.use(cors());
+app.use(cors({ credentials: true, origin: process.env.CLIENT_URL }));
 
 const port = process.env.PORT || 3030;
 
