98ea175d0e7f7dacb0f5fb70fecb070b67d2c8e2
Octavio Cossy Torquati <octaviocossytorquati@gmail.com>
2024-02-13 14:26:32 -0300

    refactor: useTheme hook

--- client/src/modules/hooks/useTheme.hook.tsx
+++ client/src/modules/hooks/useTheme.hook.tsx
@@ -1,7 +1,14 @@
 import { useTheme as useNextTheme } from 'next-themes';
+import { useEffect, useState } from 'react';
 
 export const useTheme = () => {
-  const { theme, systemTheme } = useNextTheme();
+  const [theme, setTheme] = useState<'dark' | 'light' | undefined>(undefined);
 
-  return { theme: (theme === 'system' ? systemTheme : theme) as 'dark' | 'light' | undefined };
+  const { theme: nextTheme, systemTheme } = useNextTheme();
+
+  useEffect(() => {
+    setTheme(nextTheme === 'system' ? systemTheme : (nextTheme as 'dark' | 'light'));
+  }, [nextTheme, systemTheme, theme]);
+
+  return { theme };
 };
